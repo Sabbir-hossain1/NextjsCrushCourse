97831c1a67add6a58a7458b45e815bc935ace265
sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2024-01-09 19:12:55 +0600

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lemon, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const lemon = Lemon({ subsets: ['latin'], weight: '400' });
